4282ed3e3894ad434f02288394171da4972828cf
Kseniya Shychko <k.shychko@gmail.com>
2020-02-14 01:25:47 +0300

    <feature> add StopAfterFirstMatch and IgnoreSubtreeAfterMatch options
    fixes #22

---
+++ freemarker-wrapper/src/main/java/io/codeontap/freemarkerwrapper/files/methods/list/GetLayerListMethod.java
@@ -0,0 +1,25 @@
+package io.codeontap.freemarkerwrapper.files.methods.list;
+
+import freemarker.core.Environment;
+import freemarker.template.*;
+import io.codeontap.freemarkerwrapper.RunFreeMarkerException;
+import io.codeontap.freemarkerwrapper.files.meta.LayerMeta;
+import io.codeontap.freemarkerwrapper.files.processors.LayerProcessor;
+
+import javax.json.JsonArray;
+
+public abstract class GetLayerListMethod {
+
+    protected LayerMeta meta;
+    protected LayerProcessor layerProcessor;
+
+    public TemplateModel process() {
+        JsonArray result = null;
+        try {
+            result = layerProcessor.getLayers(meta);
+        } catch (RunFreeMarkerException e) {
+            e.printStackTrace();
+        }
+        return new SimpleSequence(result, Environment.getCurrentEnvironment().getConfiguration().getObjectWrapper());
+    }
+}
--- freemarker-wrapper/src/main/java/io/codeontap/freemarkerwrapper/files/methods/tree/plugin/GetPluginTreeMethod.java
+++ freemarker-wrapper/src/main/java/io/codeontap/freemarkerwrapper/files/methods/tree/plugin/GetPluginTreeMethod.java
@@ -2,104 +2,23 @@ package io.codeontap.freemarkerwrapper.files.methods.tree.plugin;
 
 import freemarker.core.Environment;
 import freemarker.template.*;
-import io.codeontap.freemarkerwrapper.files.adapters.JsonStringAdapter;
 import io.codeontap.freemarkerwrapper.files.meta.plugin.PluginMeta;
+import io.codeontap.freemarkerwrapper.files.methods.tree.GetLayerTreeMethod;
 import io.codeontap.freemarkerwrapper.files.processors.plugin.PluginProcessor;
-import io.codeontap.freemarkerwrapper.RunFreeMarkerException;
 
-import javax.json.JsonObject;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
-public class GetPluginTreeMethod implements TemplateMethodModelEx {
+public class GetPluginTreeMethod extends GetLayerTreeMethod implements TemplateMethodModelEx {
+
 
     public TemplateModel exec(List args) throws TemplateModelException {
-        if (args.size() != 2) {
-            throw new TemplateModelException("Wrong arguments");
-        }
+        meta = new PluginMeta();
+        super.parseArguments(args);
 
         List<String> pluginLayers = (List<String>) ((DefaultListAdapter) Environment.getCurrentEnvironment().getGlobalVariable("pluginLayers")).getWrappedObject();
-        Object startingPathObj = args.get(0);
-        String startingPath = null;
-        if (startingPathObj instanceof SimpleScalar){
-            startingPath = startingPathObj.toString();
-        }else if (startingPathObj instanceof JsonStringAdapter){
-            startingPath = ((JsonStringAdapter) startingPathObj).getAsString();
-        }
-        TemplateHashModelEx options = (TemplateHashModelEx)args.get(1);
-        TemplateModelIterator iterator = options.keys().iterator();
-        TemplateSequenceModel regexSequence = null;
-        SimpleScalar regexScalar = null;
-        boolean ignoreDotDirectories = Boolean.TRUE;
-        boolean ignoreDotFiles = Boolean.TRUE;
-        boolean includePluginInformation = Boolean.FALSE;
-        boolean addStartingWildcard = Boolean.TRUE;
-        boolean addEndingWildcard = Boolean.TRUE;
-        Number minDepth = null;
-        Number maxDepth = null;
+        ((PluginMeta)meta).setLayers(pluginLayers);
 
-
-        while (iterator.hasNext()){
-            TemplateModel key = iterator.next();
-            if ("Regex".equalsIgnoreCase(key.toString())){
-                Object regex = options.get(key.toString());
-                if(regex instanceof TemplateSequenceModel)
-                    regexSequence = (TemplateSequenceModel)regex;
-                else if(regex instanceof SimpleScalar)
-                    regexScalar = (SimpleScalar)regex;
-            } else if ("IgnoreDotDirectories".equalsIgnoreCase(key.toString())){
-                ignoreDotDirectories = ((TemplateBooleanModel) options.get(key.toString())).getAsBoolean();
-            } else if ("IgnoreDotFiles".equalsIgnoreCase(key.toString())){
-                ignoreDotFiles = ((TemplateBooleanModel) options.get(key.toString())).getAsBoolean();
-            } else if ("IncludePluginInformation".equalsIgnoreCase(key.toString())){
-                includePluginInformation = ((TemplateBooleanModel) options.get(key.toString())).getAsBoolean();
-            } else if ("AddStartingWildcard".equalsIgnoreCase(key.toString())){
-                addStartingWildcard = ((TemplateBooleanModel) options.get(key.toString())).getAsBoolean();
-            } else if ("AddEndingWildcard".equalsIgnoreCase(key.toString())){
-                addEndingWildcard = ((TemplateBooleanModel) options.get(key.toString())).getAsBoolean();
-            } else if ("MinDepth".equalsIgnoreCase(key.toString())){
-                minDepth = ((TemplateNumberModel) options.get(key.toString())).getAsNumber();
-            } else if ("MaxDepth".equalsIgnoreCase(key.toString())){
-                maxDepth = ((TemplateNumberModel) options.get(key.toString())).getAsNumber();
-            }
-        }
-        List<String> regexList = new ArrayList<>();
-        if(regexSequence == null || regexSequence.size() == 0){
-            if(regexScalar == null) {
-                regexList.add("^.*$");
-            } else {
-                regexList.add(regexScalar.getAsString());
-            }
-        } else {
-            for (int i=0; i < regexSequence.size();i++){
-                regexList.add(regexSequence.get(i).toString());
-            }
-        }
-
-        PluginProcessor pluginProcessor = new PluginProcessor();
-        Set<JsonObject> result = null;
-        try {
-            PluginMeta meta = new PluginMeta();
-            meta.setLayers(pluginLayers);
-            meta.setStartingPath(startingPath);
-            meta.setRegexList(regexList);
-            meta.setIgnoreDotDirectories(ignoreDotDirectories);
-            meta.setIgnoreDotFiles(ignoreDotFiles);
-            meta.setIncludeInformation(includePluginInformation);
-            meta.setAddStartingWildcard(addStartingWildcard);
-            meta.setAddEndingWildcard(addEndingWildcard);
-            if (minDepth!=null){
-                meta.setMinDepth(minDepth.intValue());
-            }
-            if (maxDepth!=null){
-                meta.setMaxDepth(maxDepth.intValue());
-            }
-            result = pluginProcessor.getLayerTree(meta, false);
-        } catch (RunFreeMarkerException e) {
-            e.printStackTrace();
-        }
-
-        return new SimpleSequence(result, Environment.getCurrentEnvironment().getConfiguration().getObjectWrapper());
+        layerProcessor = new PluginProcessor();
+        return super.process();
     }
 }
