3ff66053b3e174cb7deb3094811362179e94ed8b
Kseniya Shychko <k.shychko@gmail.com>
2021-06-09 00:42:30 +0200

    feat: hide stack trace on stop exception

--- freemarker-wrapper/src/main/java/io/hamlet/freemarkerwrapper/WrapperTemplateExceptionHandler.java
+++ freemarker-wrapper/src/main/java/io/hamlet/freemarkerwrapper/WrapperTemplateExceptionHandler.java
@@ -5,6 +5,7 @@ import freemarker.core.StopException;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 
+import java.io.PrintWriter;
 import java.io.Writer;
 
 public class WrapperTemplateExceptionHandler implements TemplateExceptionHandler {
@@ -22,6 +23,7 @@ public class WrapperTemplateExceptionHandler implements TemplateExceptionHandler
 
     @Override
     public void handleTemplateException(TemplateException te, Environment env, Writer out) {
+        te.printStackTrace(System.err, true, true, true);
         if (te instanceof NumberOfArgumentsException) {
             System.exit(NUMBER_OF_ARGUMENTS_EXCEPTION_EXIT_CODE);
         } else if (te instanceof ParameterValueException) {
@@ -32,8 +34,6 @@ public class WrapperTemplateExceptionHandler implements TemplateExceptionHandler
             System.exit(IO_EXCEPTION_EXIT_CODE);
         } else if (te instanceof RunFreeMarkerException) {
             System.exit(RUN_FREEMARKER_EXCEPTION_EXIT_CODE);
-        } else if (te instanceof StopException) {
-            System.exit(STOP_EXCEPTION_EXIT_CODE);
         }
         System.exit(TEMPLATE_EXCEPTION_EXIT_CODE);
     }
